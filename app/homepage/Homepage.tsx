dbf8ba190530481efffeaaac2482604311562041
Admin <fedesch9@gmail.com>
2024-07-16 20:04:48 -0300

    asdsd

--- app/homepage/Homepage.tsx
+++ app/homepage/Homepage.tsx
@@ -1,7 +1,7 @@
 "use client"
-import { ReactElement, FunctionComponent, useState, useContext, useMemo, useEffect, useCallback } from "react"
+import { ReactElement, FunctionComponent, useState, useContext, useEffect, useCallback } from "react";
 import images from "@/public/images";
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion";
 import CustomImage from "../components/ui/image";
 import { Icons } from "../components/ui/icons";
 import { metrics } from "../constants/userMetrics";
@@ -11,12 +11,9 @@ import { PointsUpdateRequest } from "../models/IPoints";
 import { Metrics } from "../enums/IMetrics";
 import { sessionLimit } from "../constants/user";
 
-interface HomepageProps {
-
-}
+interface HomepageProps {}
 
 const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
-
     const updateUserPoints = useUpdateUserPoints();
 
     const {
@@ -25,72 +22,51 @@ const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
     } = useContext(ApplicationContext) as ApplicationContextData;
 
     const [taps, setTaps] = useState<number>(0);
-    const [isBoostTimeRetrieved, setIsBoostTimeRetrieved] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
 
-    async function handleUpdateUserPoints() {
+    const handleUpdateUserPoints = useCallback(async () => {
+        if (taps === 0) return;
 
-        // construct the data 
         const data: PointsUpdateRequest = {
             userId: userProfileInformation?.userId as string,
-            points: taps
+            points: taps,
         };
 
-        await updateUserPoints(data)
-            .then(() => {
-                // console.log(response);
-                fetchUserProfileInformation();
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
+        try {
+            await updateUserPoints(data);
+            fetchUserProfileInformation();
+        } catch (error) {
+            console.error(error);
+        }
+    }, [taps, updateUserPoints, userProfileInformation?.userId, fetchUserProfileInformation]);
 
-    useMemo(() => {
+    useEffect(() => {
         if (userProfileInformation) {
             setTaps(userProfileInformation.points ?? 0);
         }
     }, [userProfileInformation]);
 
-    const DEBOUNCE_DELAY = 1000; // Adjust the delay as needed
-
     useEffect(() => {
-        if (taps === 0) return;
-
         const timer = setTimeout(() => {
             handleUpdateUserPoints();
-        }, DEBOUNCE_DELAY);
+        }, 1000); // Save points every second
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [taps]);
+        return () => clearTimeout(timer);
+    }, [taps, handleUpdateUserPoints]);
 
-    // // const DEBOUNCE_DELAY_FOR_SESSION = 32400; // Delay for 3 clicks for 3hrs
-    // const DEBOUNCE_DELAY_FOR_SESSION = 10800; // Delay for 1 click for 3hrs
+    const handleClick = () => {
+        if (timesClickedPerSession === undefined || sessionLimit - timesClickedPerSession <= 0) return;
 
-    // // Use a hook to update the timesClickedPerSession back to zero after the user has stopped clicking. Decrement the timesclickedpersession by 3 till the limit is reached
-    // useEffect(() => {
+        setTaps(prevTaps => prevTaps + (1 * userProfileInformation.level));
+        updateTimesClickedPerSession(timesClickedPerSession + 1);
+        setIsClicked(true);
 
-    //     if (sessionLimit - timesClickedPerSession >= sessionLimit || timesClickedPerSession <= 0) {
-    //         // reset the state
-    //         setTimesClickedPerSession(0);
-    //         return;
-    //     };
+        setTimeout(() => {
+            setIsClicked(false);
+        }, 300); // Reset click animation state after 300ms
+    };
 
-    //     const timer = setTimeout(() => {
-    //         // Decrement the timesClickedPerSession by 3
-    //         setTimesClickedPerSession(timesClickedPerSession - 1);
-    //     }, DEBOUNCE_DELAY_FOR_SESSION);
-
-    //     // save the timesClickedPerSession to the session storage
-    //     sessionStorage.setItem(StorageKeys.TimesClickedPerSession, timesClickedPerSession.toString());
-
-    //     return () => {
-    //         clearTimeout(timer);
-    //     };
-    // }, [timesClickedPerSession]);
-
-    function swapColorBasedOnStatus() {
+    const swapColorBasedOnStatus = () => {
         if (metrics(taps)?.status === Metrics.NOOB) {
             return "text-green-500/60";
         } else if (metrics(taps)?.status === Metrics.BEGINNER) {
@@ -105,48 +81,38 @@ const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
             return "text-white/60";
         }
     };
-    const [isClicked, setIsClicked] = useState(false);
-    // useMemo(() => {
-    //     if (isClicked) {
-    //         setIsClicked(false);
-    //     } 
-    // }, [isClicked])
-
-    // async function _handleUpdateBoostRefillEndTime(endTime: Date) {
-    //     await updateBoostRefillEndTime({ username: userProfileInformation?.username as string, refillEndTime: endTime })
-    //         .then((response) => {
-    //             console.log("Boost refill time updated", response);
-    //         })
-    //         .catch((error) => {
-    //             console.error("Error updating boost refill time", error);
-    //         });
-    // };
 
     return (
         <main className="flex min-h-screen flex-col items-center py-20 pb-32 select-none">
-            {
-                userProfileInformation &&
+            {userProfileInformation && (
                 <>
                     <div className="flex flex-col items-center mb-12">
                         <div className="flex flex-row gap-2 items-center">
                             <span className="w-7 h-7 relative grid place-items-center">
                                 <CustomImage src={images.coin} alt="Coin" />
                             </span>
-                            <h1 className="text-[40px] text-white font-extrabold">{(taps).toLocaleString()}{metrics(taps)?.pointSuffix}</h1>
+                            <h1 className="text-[40px] text-white font-extrabold">
+                                {taps.toLocaleString()}
+                                {metrics(taps)?.pointSuffix}
+                            </h1>
                         </div>
                         <div className="flex flex-row gap-3 items-center">
                             <div className="flex flex-row gap-2 items-center">
                                 <span className="w-6 h-6 grid place-items-center">
                                     <Icons.Trophy className="opacity-40" />
                                 </span>
-                                <p className={`${swapColorBasedOnStatus()} text-sm`}>{metrics(taps)?.status}</p>
+                                <p className={`${swapColorBasedOnStatus()} text-sm`}>
+                                    {metrics(taps)?.status}
+                                </p>
                             </div>
                             <span className="h-4 w-[1px] bg-slate-50/50 block" />
                             <div className="flex flex-row gap-2 items-center">
                                 <span className="w-6 h-6 grid place-items-center">
                                     <Icons.Star />
                                 </span>
-                                <p className="text-white/60 text-sm">Level: {userProfileInformation.level}</p>
+                                <p className="text-white/60 text-sm">
+                                    Level: {userProfileInformation.level}
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -154,65 +120,46 @@ const Homepage: FunctionComponent<HomepageProps> = (): ReactElement => {
                     <div className="flex relative mb-12">
                         <div className="absolute w-full h-full flex items-center justify-end z-20 pointer-events-none">
                             <AnimatePresence>
-                                {
-                                    isClicked &&
-                                    ([...Array(10)]).map((click, index) => (
+                                {isClicked &&
+                                    [...Array(10)].map((_, index) => (
                                         <motion.div
-                                            key={`${Date.now()}${index}`}
+                                            key={index}
                                             initial={{ opacity: 1, y: 0 }}
                                             animate={{ opacity: 0, y: -200 }}
                                             exit={{ opacity: 0, y: -200 }}
                                             transition={{ duration: 1 }}
-                                            // style={{ left: click.x, top: click.y }}
                                             className="absolute z-20 text-lg text-white pr-6"
                                         >
                                             +{userProfileInformation.level}
                                         </motion.div>
-                                    ))
-                                }
+                                    ))}
                             </AnimatePresence>
                         </div>
                         <motion.span
-                            onClick={() => {
-                                if (timesClickedPerSession === undefined) return;
-
-                                if (sessionLimit - timesClickedPerSession <= 0) return;
-
-                                setTaps(taps + (1 * userProfileInformation.level));
-
-                                updateTimesClickedPerSession(timesClickedPerSession + 1);
-
-                                setIsClicked(!isClicked);
-                            }}
+                            onClick={handleClick}
                             whileTap={{
-                                // scale: 1.1,
+                                scale: 0.9,
                                 filter: "brightness(1.25)",
-                                transition: { duration: 0.1 }
+                                transition: { duration: 0.1 },
                             }}
-                            className="w-60 h-60 relative">
+                            className="w-60 h-60 relative cursor-pointer"
+                        >
                             <CustomImage priority src={images.clicker} alt="Durov" />
                         </motion.span>
                     </div>
 
-                    {
-                        timesClickedPerSession !== undefined &&
+                    {timesClickedPerSession !== undefined && (
                         <div className="flex flex-row items-center text-white mb-5">
                             <p className="text-slate-400">Energy level:</p>&nbsp;
-                            <span className="text-base">{sessionLimit - timesClickedPerSession}/{sessionLimit}</span>
+                            <span className="text-base">
+                                {sessionLimit - timesClickedPerSession}/{sessionLimit}
+                            </span>
                         </div>
-                    }
-
-                    {/* {
-                        userProfileInformation.referralCount ?
-                            <div className="flex flex-row items-center text-white">
-                                <p className="text-slate-400">Referral points:</p>&nbsp;
-                                <span className="text-xl">{(userProfileInformation.referralCount * 1000).toLocaleString()}</span>
-                            </div> : <></>
-                    } */}
+                    )}
                 </>
-            }
+            )}
         </main>
     );
-}
+};
 
 export default Homepage;
