a6cbb62852af4b8af882bb7c7ff39d0f3d3e5934
Admin <fedesch9@gmail.com>
2024-07-18 01:09:47 -0300

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,8 +68,9 @@ const Home = () => {
 
 
 
-            <div id="footermain" className={`flex flex-col bg-[#0e2273;] space-y-6 fixed bottom-0 py-6 left-0 right-0 justify-center items-center px-5`}>
+        
 
+            <div id="footermain" className={`flex flex-col bg-[#1a1f2e] space-y-6 fixed bottom-0 py-6 left-0 right-0 justify-center items-center px-5 rounded-t-2xl`}>
 
            <Footer/>
            </div>
