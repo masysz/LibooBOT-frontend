01989f35db228302f6cd4f180896360a91539029
Mr Singh <66018893+MrSIngh76961@users.noreply.github.com>
2024-07-21 01:40:59 +0530

    Update donate.js

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -23,8 +23,32 @@ const Donate = () => {
     if (window.Telegram?.WebApp) {
       window.Telegram.WebApp.ready();
       window.Telegram.WebApp.expand();
+      
     }
   }, []);
+
+  useEffect(()=>{
+     function ensureDocumentIsScrollable() {
+        const isScrollable =
+          document.documentElement.scrollHeight > window.innerHeight;
+        if (!isScrollable) {
+          document.documentElement.style.setProperty(
+            "height",
+            "calc(100vh + 1px)",
+            "important"
+          );
+        }
+      }
+      function preventCollapse() {
+        if (window.scrollY === 0) {
+          window.scrollTo(0, 1);
+        }
+      }
+
+      scrollRef.addEventListener("touchstart", preventCollapse);
+
+      window.addEventListener("load", ensureDocumentIsScrollable);
+  },[])
 
   const fetchCampaigns = useCallback(async () => {
     setIsLoading(true);
