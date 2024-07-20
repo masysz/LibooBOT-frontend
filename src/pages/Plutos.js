7d9c216843cdf60315d02dab37eb60ba0cf7571d
Admin <fedesch9@gmail.com>
2024-07-20 01:03:27 -0300

    changes

--- src/pages/Plutos.js
+++ src/pages/Plutos.js
@@ -354,12 +354,17 @@ const Plutos = () => {
 
 
   const formatNumber = (num) => {
-    if (num < 1000000) {
+    if (num < 100000) {
       return new Intl.NumberFormat().format(num).replace(/,/g, " ");
+    } else if (num < 1000000) {
+      return new Intl.NumberFormat().format(num).replace(/,/g, "M");
+    // } else {
+    //   return (num / 1000000).toFixed(3).replace(".", ".") + " M";
     } else {
-      return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+      return new Intl.NumberFormat().format(num).replace(/,/g, " ");
     }
   };
+
       // // Remove the click after the animation duration
       // setTimeout(() => {
       //   setTapGuru(false);
@@ -375,75 +380,57 @@ const Plutos = () => {
             <Spinner />
           ) : (
             <Animate>
-              <div className="w-full flex justify-center flex-col items-center overflow-hidden">
-                <div className="flex space-x-[2px] justify-center items-center">
-                  <div className="w-[50px] h-[50px]">
-                    <img src={coinsmall} className="w-full" alt="coin" />
+              <div className="w-full h-screen flex flex-col items-center justify-between py-8">
+                <div className="flex flex-col items-center">
+                  <div className="flex items-center space-x-2">
+                    <img src={coinsmall} className="w-[40px] h-[40px]" alt="coin" />
+                    <h1 className="text-[#fff] text-[36px] font-extrabold">
+                      {formatNumber(balance + refBonus)}
+                    </h1>
                   </div>
-                  <h1 className="text-[#fff] text-[42px] font-extrabold">
-                    {formatNumber(balance + refBonus)} <br />
-                  </h1>
+                  <div className="flex items-center space-x-1 mt-2">
+                    <img
+                      src={level.imgUrl}
+                      className="w-[20px] h-[20px]"
+                      alt="level"
+                    />
+                    <h2 onClick={() => setShowLevels(true)} className="text-[#9d99a9] text-[16px] font-medium">
+                      {level.name}
+                    </h2>
+                    <MdOutlineKeyboardArrowRight className="w-[16px] h-[16px] text-[#9d99a9]" />
+                  </div>
                 </div>
-                <div className="w-full ml-[6px] flex space-x-1 items-center justify-center">
-                  <img
-                    src={level.imgUrl}
-                    className="w-[25px] relative"
-                    alt="bronze"
-                  />
-                  <h2 onClick={() => setShowLevels(true)} className="text-[#9d99a9] text-[20px] font-medium">
-                    {level.name}
-                  </h2>
-                  <MdOutlineKeyboardArrowRight className="w-[20px] h-[20px] text-[#9d99a9] mt-[2px]" />
-                </div>
-                <div className="w-full flex justify-center items-center relative">
-                  <div className="bg-[#0077cc] blur-[50px] absolute w-[200px] h-[220px] rounded-full mb-[70px]"></div>
+    
+                <div className="flex-1 flex justify-center items-center relative">
+                  <div className="bg-[#0077cc] blur-[50px] absolute w-[200px] h-[220px] rounded-full"></div>
                   <div className={`${tapGuru ? 'block' : 'hidden'} pyro`}>
                     <div className="before"></div>
                     <div className="after"></div>
                   </div>
-                  <div className="w-[350px] h-[350px] relative flex items-center justify-center">
+                  <div className="relative">
                     <img
                       src="/lihgt.gif"
-                      alt="err"
-                      className={`absolute w-[350px] rotate-45 mb-[100px] ${tapGuru ? 'block' : 'hidden'}`}
+                      alt="background"
+                      className={`absolute w-[350px] rotate-45 ${tapGuru ? 'block' : 'hidden'}`}
                     />
-                    <div className="image-container">
-                      {mainTap && (
-                        <Container>
-                          <img 
-                            onPointerDown={handleClick}
-                            ref={imageRef}
-                            src={level.imgTap}
-                            alt="Wobble"
-                            className="wobble-image !w-[250px] select-none"
-                          />
-                          {clicks.map((click) => (
-                            <SlideUpText key={click.id} x={click.x} y={click.y}>
-                              +{tapValue.value}
-                            </SlideUpText>
-                          ))}
-                        </Container>
-                      )}
-                      {tapGuru && (
-                        <Container>
-                          <img
-                            onPointerDown={handleClickGuru}
-                            ref={imageRef}
-                            src={level.imgBoost}
-                            alt="Wobble"
-                            className="wobble-image !w-[250px] select-none"
-                          />
-                          {clicks.map((click) => (
-                            <SlideUpText key={click.id} x={click.x} y={click.y}>
-                              +{tapValue.value * 5}
-                            </SlideUpText>
-                          ))}
-                        </Container>
-                      )}
-                    </div>
+                    <Container>
+                      <img 
+                        onPointerDown={mainTap ? handleClick : handleClickGuru}
+                        ref={imageRef}
+                        src={mainTap ? level.imgTap : level.imgBoost}
+                        alt="Tap"
+                        className="wobble-image !w-[250px] select-none"
+                      />
+                      {clicks.map((click) => (
+                        <SlideUpText key={click.id} x={click.x} y={click.y}>
+                          +{mainTap ? tapValue.value : tapValue.value * 5}
+                        </SlideUpText>
+                      ))}
+                    </Container>
                   </div>
                 </div>
-                <div className="flex flex-col space-y-6 fixed bottom-[120px] left-0 right-0 justify-center items-center px-5">
+    
+                <div className="w-full px-5 mb-[100px] mt-[-70px]">
                   <div className="flex flex-col w-full items-center justify-center">
                     <div className="flex pb-[6px] space-x-1 items-center justify-center text-[#fff]">
                       <img alt="flash" src={flash} className="w-[20px]" />
@@ -460,11 +447,13 @@ const Plutos = () => {
                     </div>
                   </div>
                 </div>
+    
                 <Levels showLevels={showLevels} setShowLevels={setShowLevels} />
               </div>
             </Animate>
           )}
         </>
-      )};
-
-export default Plutos;
+      );
+    };
+    
+    export default Plutos;
