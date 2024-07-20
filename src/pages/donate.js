2b13e0c90ccc5d6d5b91b601c8311eb97ac63fa0
Mr Singh <66018893+MrSIngh76961@users.noreply.github.com>
2024-07-21 01:48:40 +0530

    Update donate.js

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -27,29 +27,7 @@ const Donate = () => {
     }
   }, []);
 
-  useEffect(()=>{
-     function ensureDocumentIsScrollable() {
-        const isScrollable =
-          document.documentElement.scrollHeight > window.innerHeight;
-        if (!isScrollable) {
-          document.documentElement.style.setProperty(
-            "height",
-            "calc(100vh + 1px)",
-            "important"
-          );
-        }
-      }
-      function preventCollapse() {
-        if (window.scrollY === 0) {
-          window.scrollTo(0, 1);
-        }
-      }
-
-     const scrollableElement = document.getElementById("scrollable-element");
-      scrollableElement.addEventListener("touchstart", preventCollapse);
-
-      window.addEventListener("load", ensureDocumentIsScrollable);
-  },[])
+  
 
   const fetchCampaigns = useCallback(async () => {
     setIsLoading(true);
