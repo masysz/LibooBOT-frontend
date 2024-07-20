9549f5ee7d97b4c4bcad47886672018400e47360
Mr Singh <66018893+MrSIngh76961@users.noreply.github.com>
2024-07-21 00:52:08 +0530

    Update donate.js

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -23,6 +23,8 @@ const Donate = () => {
     if (window.Telegram?.WebApp) {
       window.Telegram.WebApp.ready();
       window.Telegram.WebApp.expand();
+      window.Telegram.WebApp.disableVerticalSwipes();
+
     }
   }, []);
 
@@ -188,7 +190,7 @@ const Donate = () => {
 
   return (
     <Animate>
-      <div className="w-full h-full flex flex-col	" style={{ height: '100vh' }}>
+      <div className="w-full h-full flex flex-col	overflow-y-auto" style={{ height: '100vh' }}>
         <div className="w-full absolute top-[-35px] left-0 right-0 flex justify-center z-20 pointer-events-none select-none">
           {congrats ? <img src={congratspic} alt="congrats" className="w-[80%]" /> : null}
         </div>
