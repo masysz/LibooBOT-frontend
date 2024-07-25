79405837bd985a3533114453d418fc32306064d0
Admin <fedesch9@gmail.com>
2024-07-25 20:18:24 -0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
           red: "#d6436e",
           green: "#25da72",
           mycolor: "#3c5b8038",
-          cards: "#abc8ff",
+          cards: "#d6e4ff",
           energybar: "#32374e",
           btn: "#0b62c4",
           btn2: "#0b62c421",
