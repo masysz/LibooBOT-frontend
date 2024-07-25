85febe35e21bdb753c271e45d41aa5d6cb6fec52
Admin <fedesch9@gmail.com>
2024-07-25 17:37:57 -0300

    add preloader

--- src/_codux/boards/donate/donate.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createBoard } from "@wixc3/react-board";
-import Donate from "../../../pages/donate";
-
-export default createBoard({
-  name: "Donate",
-  Board: () => <Donate />,
-  isSnippet: true,
-});
