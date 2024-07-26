2f0ce5385c36a964799a151613a634c7bf411b6e
Admin <fedesch9@gmail.com>
2024-07-26 13:38:45 -0300

    changes

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -3,7 +3,7 @@ import Animate from "../Components/Animate";
 import { Outlet } from "react-router-dom";
 import ClaimLeveler from "../Components/ClaimLeveler";
 import Spinner from "../Components/Spinner";
-import coinsmall from "../images/coinsmall.webp";
+import coinsmall from "../images/main-logo.png";
 import { useUser } from "../context/userContext";
 
 
@@ -76,7 +76,7 @@ const Ref = () => {
 
           <div className="w-full bg-cards rounded-[12px] px-3 py-4 flex flex-col">
             <span className="w-full flex justify-between items-center pb-2">
-              <h2 className="text-[18px] font-semibold">My invite link:</h2>
+              <h2 className="text-[18px] font-semibold text-[#262626]">My invite link:</h2>
               <span
                onClick={copyToClipboard}
                 className="bg-gradient-to-b from-[#094e9d] to-[#0b62c4] font-medium py-[6px] px-4 rounded-[12px] flex items-center justify-center text-[16px]"
@@ -84,21 +84,21 @@ const Ref = () => {
               {copied ? <span>Copied!</span> : <span>Copy</span>}
               </span>
             </span>
-            <div className="text-[#9a96a6] text-[13px]">
+            <div className="text-[#262626] text-[13px]">
             https://t.me/liboo_tonbot?start=r{id}
             </div>
           </div>
           <div className="bg-borders w-full px-5 h-[1px] !mt-6"></div>
 
           <div className="w-full flex flex-col">
-            <h3 className="text-[22px] font-semibold pb-[16px]">My Referrals:</h3>
+            <h3 className="text-[22px] font-semibold pb-[16px] text-[#262626]">My Referrals:</h3>
 
             <div className="w-full flex flex-col space-y-3">
 
             {loading ? (
         <p className='w-full text-center'>checking...</p>
       ) : referrals.length === 0 ? (
-        <p className='text-center w-full now pt-8 px-5 text-[14px] leading-[24px]'>
+        <p className='text-center w-full now pt-8 px-5 text-[14px] text-[#262626] leading-[24px]'>
          You don't have referrals😭
           </p>
       ) : (
@@ -115,11 +115,11 @@ const Ref = () => {
                       className="bg-cards rounded-[10px] p-[14px] flex flex-wrap justify-between items-center"
                     >
                       <div className="flex flex-1 flex-col space-y-1">
-                        <div className="text-[#fff] pl-1 text-[16px] font-semibold">
+                        <div className="text-[#262626] pl-1 text-[16px] font-semibold">
                         {user.username}
                         </div>
 
-                        <div className="flex items-center space-x-1 text-[14px] text-[#e5e5e5]">
+                        <div className="flex items-center space-x-1 text-[14px] text-[#507cff]">
                           <div className="">
                             <img src={user.level.imgUrl} alt="bronze" className="w-[18px]" />
                           </div>
@@ -135,13 +135,13 @@ const Ref = () => {
                               alt="coin"
                             />
                           </span>
-                          <span className="font-normal text-[#ffffff] text-[15px]">
+                          <span className="font-normal text-[#507cff] text-[15px]">
                           {formatNumber(user.balance)}
                           </span>
                         </div>
                       </div>
 
-                      <div className="text-[#ffce68] font-semibold text-[14px]">
+                      <div className="text-[#ffce68] font-semibold text-[15px]">
                       +{formatNumber(user.balance / 100 * 10)}
                       </div>
                       <div className="flex w-full mt-2 p-[4px] items-center bg-energybar rounded-[10px] border-[1px] border-borders">
