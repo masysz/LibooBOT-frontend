166f6377cc39593afa6f91dc271394e3fcb9ad21
Admin <fedesch9@gmail.com>
2024-07-27 15:34:38 -0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,9 +52,7 @@ module.exports = {
     },
     plugins: [
       require('tailwindcss'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/aspect-ratio')
+  
     ],
     
   };
