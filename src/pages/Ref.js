e8a1d60b460fc126d6ac14b4f57d940c44fa0f61
Admin <fedesch9@gmail.com>
2024-07-26 22:28:26 -0300

    changes

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -10,7 +10,7 @@ import { useUser } from "../context/userContext";
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 70vh;
+  height: 85vh;
   overflow: hidden;
 `;
 
