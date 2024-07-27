47b3240e62080b83a5247e874ec1c648847ae203
Admin <fedesch9@gmail.com>
2024-07-27 17:51:47 -0300

    changes on footer

--- src/Components/energybar.js
+++ src/Components/energybar.js
@@ -4,28 +4,27 @@ import { NavLink } from "react-router-dom";
 const EnergyBar = ({ energy, battery, energyPercentage, flash, leaderboard }) => {
   return (
     <div className="w-full fixed bottom-[120px] left-0 right-0 px-5">
-      <div className="flex w-full items-end justify-between bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-2xl p-3">
+      <div className="flex w-full items-center justify-between bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-2xl p-2">
         <div className="flex flex-col w-[70%]">
-          <div className="flex pb-3 space-x-2 items-center text-[#fff]">
-            <img alt="flash" src={flash} className="w-[22px]" />
-            <div>
-              <span className="text-[16px] font-bold">{energy.toFixed(0)}</span>
-              <span className="text-[16px] font-medium ml-1">/ {battery.energy}</span>
+          <div className="flex items-center text-white mb-2">
+            <img alt="flash" src={flash} className="w-[22px] mr-2" />
+            <div className="relative w-full h-[18px] bg-energybar rounded-full border border-borders2">
+              <div
+                className="bg-[#3f88e8] h-full rounded-full transition-width duration-100"
+                style={{ width: `${energyPercentage}%` }}
+              ></div>
+              <div className="absolute inset-0 flex items-center justify-center text-[12px] font-bold text-white">
+                {energy.toFixed(0)} / {battery.energy}
+              </div>
             </div>
-          </div>
-          <div className="flex w-full p-[2px] h-[18px] items-center bg-energybar rounded-[12px] border-[1px] border-borders2">
-            <div
-              className="bg-[#3f88e8] h-full rounded-full transition-width duration-100"
-              style={{ width: `${energyPercentage}%` }}
-            ></div>
           </div>
         </div>
         <NavLink 
           to="/tapsleaderboard" 
-          className="w-[22%] flex items-center justify-center space-x-2 bg-white bg-opacity-30 rounded-xl p-2 transition-all duration-300 hover:bg-opacity-50"
+          className="w-[22%] flex items-center justify-center bg-white bg-opacity-30 rounded-xl p-2 transition-all duration-300 hover:bg-opacity-50"
         >
-          <img src={leaderboard} alt="Leaderboard" className="w-5 h-5" />
-          <span className="text-[11px] px-1 text-white font-semibold whitespace-nowrap">Leaderboard</span>
+          <img src={leaderboard} alt="Leaderboard" className="w-5 h-5 mr-2" />
+          <span className="text-[11px] text-white font-semibold whitespace-nowrap">Leaderboard</span>
         </NavLink>
       </div>
     </div>
